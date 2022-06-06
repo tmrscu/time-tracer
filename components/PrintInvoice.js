f0aece408f266fb500f532dfe32daa4659e931ac
MatthewAMartin <mattmartin.2289@gmail.com>
2022-06-06 22:25:35 +0900

    Print Invoice page formatting

--- components/PrintInvoice.js
+++ components/PrintInvoice.js
@@ -1,10 +1,4 @@
 import {
-  Container,
-  Flex,
-  Button,
-  Heading,
-  useDisclosure,
-  Box,
   TableContainer,
   Table,
   Thead,
@@ -12,33 +6,86 @@ import {
   Tr,
   Th,
   Td,
-  Link,
+  UnorderedList,
+  ListItem,
+  Grid,
+  GridItem,
+  Heading,
 } from "@chakra-ui/react";
-
-import React from 'react'
+import { useState, useEffect } from "react";
+import React from "react";
 
 // eslint-disable-next-line react/display-name
-const PrintInvoice = React.forwardRef((props, ref) => (
-    <TableContainer ref={ref}>
-      <Table variant="simple">
+const PrintInvoice = React.forwardRef((props, ref) => {
+  return (
+    <TableContainer ref={ref} padding={30}>
+      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+        <GridItem w="100%">
+          <Heading>INVOICE</Heading>
+        </GridItem>
+        <GridItem w="100%">
+          <UnorderedList
+            fontWeight={500}
+            fontSize={"16px"}
+            lineHeight={"25px"}
+            color={"#333"}
+            listStyleType={"none"}
+            textAlign={"right"}
+            mb={10}
+          >
+            <ListItem>Invoice ID: {props.invoiceData.invoice_id}</ListItem>
+            <ListItem>Start Date: {props.invoiceData.start_date}</ListItem>
+            <ListItem>End Date: {props.invoiceData.end_date}</ListItem>
+          </UnorderedList>
+          <UnorderedList
+            fontWeight={500}
+            fontSize={"16px"}
+            lineHeight={"25px"}
+            color={"#333"}
+            listStyleType={"none"}
+            textAlign={"right"}
+            mb={10}
+          >
+            <ListItem>{props.clientData.company}</ListItem>
+            <ListItem>
+              {props.clientData.first_name} {props.clientData.last_name}
+            </ListItem>
+            <ListItem>{props.clientData.email}</ListItem>
+          </UnorderedList>
+        </GridItem>
+      </Grid>
+
+      <Table variant="unstyled" style={{ layout: "fixed" }}>
         <Thead>
-          <Tr>
-            <Th>Item</Th>
-            <Th>Duration</Th>
-            <Th>Price</Th>
+          <Tr bg={"#eee"} fontWeight={700} borderBottom={"1px solid #ddd"}>
+            <Th width={"33%"}>Item</Th>
+            <Th width={"33%"} textAlign={"center"}>
+              Duration
+            </Th>
+            <Th width={"33%"} textAlign={"right"}>
+              Price
+            </Th>
           </Tr>
         </Thead>
         <Tbody>
-          {props.items.map((item) => (
-            <Tr key={item.id}>
+          {props.items.map((item, index) => (
+            <Tr key={index} borderBottom={"1px solid #eee"}>
               <Td>{item.task_type}</Td>
-              <Td>{item.duration}</Td>
-              <Td>{item.hourly_rate}</Td>
+              <Td textAlign={"center"}>{item.duration}</Td>
+              <Td textAlign={"right"}>{item.price}</Td>
             </Tr>
           ))}
+          <Tr borderTop={"2px solid #eee;"}>
+            <Td></Td>
+            <Td></Td>
+            <Td fontWeight={700} textAlign={"right"}>
+              Total: {props.invoiceTotal}
+            </Td>
+          </Tr>
         </Tbody>
       </Table>
     </TableContainer>
-));
+  );
+});
 
 export default PrintInvoice;
